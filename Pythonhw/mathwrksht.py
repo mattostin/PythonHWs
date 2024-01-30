5c940b49d878d42cb79749676f6fc2cf4eeeb856
Matthew Ostin <matthewositn@gwu.edu>
2024-01-30 15:03:14 -0500

    comments

--- Pythonhw/mathwrksht.py
+++ Pythonhw/mathwrksht.py
@@ -1,10 +1,13 @@
+#Bringing the math library 
 import math
 
+#Creating the inputs and asking for the params
 var_radius= float(input("Enter an radius: "))
 var_height= float(input("Enter an height: "))
 
-
+#The variable responsible for calculating the surface area
 math_calculator = 2 * math.pi * var_radius * var_height + 2 \
     * math.pi * var_radius ** 2
 
+#Printing the user's inputs
 print(math_calculator)
